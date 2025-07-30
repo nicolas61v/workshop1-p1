183bffe1d063e6d04aa2bfac0229a2ce508a0e66
nicolas61v <vasquezjuannicolas71@gmail.com>
2025-07-30 10:19:59 -0500

    estructura de carpetas corregidas

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'moviereviews_new.urls'
+ROOT_URLCONF = 'moviereviews.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'moviereviews_new.wsgi.application'
+WSGI_APPLICATION = 'moviereviews.wsgi.application'
 
 
 # Database
