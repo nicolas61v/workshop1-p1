17cb147ba340c69528db566df790b8e6a5b40a5c
nicolas61v <vasquezjuannicolas71@gmail.com>
2025-07-30 10:35:30 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name':'Nicolas Vasquez'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
