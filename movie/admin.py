0d5958e475740d6a9f4b48efeadbc6af5dde453e
nicolas61v <vasquezjuannicolas71@gmail.com>
2025-07-30 11:16:35 -0500

    creted improvements for desing

--- movie/admin.py
+++ movie/admin.py
@@ -1,5 +1,10 @@
+
 from django.contrib import admin
 from .models import Movie
 
-# Register your models here.
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'url')
+    search_fields = ('title', 'description')
+
+admin.site.register(Movie, MovieAdmin)
+
--- movie/models.py
+++ movie/models.py
@@ -1,8 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+
+class Review(models.Model):
+    text = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    watch_again = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
